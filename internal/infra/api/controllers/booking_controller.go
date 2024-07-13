91d02da51383a93651494582c7175ff400064d88
Juliano Silva <cont.juliano@outlook.com>
2024-07-13 11:07:13 -0300

    docs: updated swagger

--- internal/infra/api/controllers/booking_controller.go
+++ internal/infra/api/controllers/booking_controller.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 
 	"github.com/julianojj/fastrip/internal/core/exceptions"
@@ -37,20 +36,20 @@ func NewBookingController(
 //	@Tags			booking
 //	@Accept			json
 //	@Produce		json
-//	@Param			input	body		usecases.MakeBookingInput	true	"MakeBookingInput"
-//	@Success		201		{array}		usecases.MakeBookingOutput
-//	@Failure		400		{object}	ErrorResponse
-//	@Failure		404		{object}	ErrorResponse
-//	@Failure		422		{object}	ErrorResponse
-//	@Failure		500		{object}	ErrorResponse
+//	@Param			Authorization	header		string						true	"Insert you bearer token"
+//	@Param			input			body		usecases.MakeBookingInput	true	"MakeBookingInput"
+//	@Success		201				{array}		usecases.MakeBookingOutput
+//	@Failure		400				{object}	ErrorResponse
+//	@Failure		404				{object}	ErrorResponse
+//	@Failure		422				{object}	ErrorResponse
+//	@Failure		500				{object}	ErrorResponse
 //	@Router			/make_booking [post]
 func (c *BookingController) MakeBooking(w http.ResponseWriter, r *http.Request) {
 	var input usecases.MakeBookingInput
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		handleError(w, http.StatusBadRequest, "Invalid request body", err)
+		handleError(w, http.StatusBadRequest, "Invalid request body")
 		return
 	}
-
 	output, err := c.makeBooking.Execute(&input)
 	if err != nil {
 		statusCode := http.StatusInternalServerError
@@ -60,19 +59,28 @@ func (c *BookingController) MakeBooking(w http.ResponseWriter, r *http.Request) 
 		case *exceptions.NotFoundException:
 			statusCode = http.StatusNotFound
 		}
-
-		handleError(w, statusCode, err.Error(), err)
+		handleError(w, statusCode, err.Error())
 		return
 	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	if err := json.NewEncoder(w).Encode(&output); err != nil {
-		handleError(w, http.StatusInternalServerError, "Failed to encode response", err)
+		handleError(w, http.StatusInternalServerError, "Failed to encode response")
 		return
 	}
 }
 
+// GetRooms godoc
+//
+//	@Summary		Pegar reservas
+//	@Description	Pegar reservas
+//	@Tags			booking
+//	@Produce		json
+//	@Param			Authorization	header		string	true	"Insert you bearer token"
+//	@Success		200				{array}		usecases.GetBookingsOutput
+//	@Failure		422				{object}	ErrorResponse
+//	@Failure		500				{object}	ErrorResponse
+//	@Router			/get_bookings [get]
 func (c *BookingController) GetBookings(w http.ResponseWriter, r *http.Request) {
 	output, err := c.getBookings.Execute()
 	if err != nil {
@@ -80,28 +88,23 @@ func (c *BookingController) GetBookings(w http.ResponseWriter, r *http.Request) 
 		switch err.(type) {
 		case *exceptions.DomainException:
 			statusCode = http.StatusUnprocessableEntity
-		case *exceptions.NotFoundException:
-			statusCode = http.StatusNotFound
 		}
-
-		handleError(w, statusCode, err.Error(), err)
+		handleError(w, statusCode, err.Error())
 		return
 	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(&output); err != nil {
-		handleError(w, http.StatusInternalServerError, "Failed to encode response", err)
+		handleError(w, http.StatusInternalServerError, "Failed to encode response")
 		return
 	}
 }
 
-func handleError(w http.ResponseWriter, statusCode int, message string, err error) {
+func handleError(w http.ResponseWriter, statusCode int, message string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(&ErrorResponse{
 		Message: message,
 		Code:    statusCode,
 	})
-	log.Printf("Error %d: %s - %v", statusCode, message, err)
 }
--- internal/infra/api/controllers/user_controller.go
+++ internal/infra/api/controllers/user_controller.go
@@ -10,13 +10,57 @@ import (
 
 type UserController struct {
 	registerUser *usecases.RegisterUser
+	authUser     *usecases.AuthUser
 }
 
 func NewUserController(
 	registerUser *usecases.RegisterUser,
+	authUser *usecases.AuthUser,
 ) *UserController {
 	return &UserController{
 		registerUser,
+		authUser,
+	}
+}
+
+// Authenticate user godoc
+//
+//	@Summary		Autentica usuário
+//	@Description	Autentica usuário
+//	@Tags			user
+//	@Accept			json
+//	@Produce		json
+//	@Param			input	body		usecases.AuthUserInput	true	"AuthUserInput"
+//	@Success		200		{object}	usecases.AuthUserOutput
+//	@Failure		401		{object}	ErrorResponse
+//	@Failure		500		{object}	ErrorResponse
+//	@Router			/auth_user [post]
+func (c *UserController) AuthenticateUser(w http.ResponseWriter, r *http.Request) {
+	var input *usecases.AuthUserInput
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		handleError(w, http.StatusBadRequest, "Invalid request body")
+		return
+	}
+
+	output, err := c.authUser.Execute(input)
+	if err != nil {
+		statusCode := http.StatusInternalServerError
+		switch err.(type) {
+		case *exceptions.DomainException:
+			statusCode = http.StatusUnprocessableEntity
+		case *exceptions.UnauthorizedException:
+			statusCode = http.StatusUnauthorized
+		}
+
+		handleError(w, statusCode, err.Error())
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(&output); err != nil {
+		handleError(w, http.StatusInternalServerError, "Failed to encode response")
+		return
 	}
 }
 
@@ -36,7 +80,7 @@ func NewUserController(
 func (c *UserController) RegisterUser(w http.ResponseWriter, r *http.Request) {
 	var input *usecases.RegisterUserInput
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		handleError(w, http.StatusBadRequest, "Invalid request body", err)
+		handleError(w, http.StatusBadRequest, "Invalid request body")
 		return
 	}
 
@@ -47,13 +91,13 @@ func (c *UserController) RegisterUser(w http.ResponseWriter, r *http.Request) {
 		case *exceptions.DomainException:
 			statusCode = http.StatusUnprocessableEntity
 		}
-		handleError(w, statusCode, err.Error(), err)
+		handleError(w, statusCode, err.Error())
 		return
 	}
 
 	w.WriteHeader(http.StatusCreated)
 	if err := json.NewEncoder(w).Encode(&output); err != nil {
-		handleError(w, http.StatusInternalServerError, "Failed to encode response", err)
+		handleError(w, http.StatusInternalServerError, "Failed to encode response")
 		return
 	}
 }
